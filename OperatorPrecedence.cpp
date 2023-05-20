60b7c4edf7931b7c9c6a90e010c3f10f064de4ff
bhanupjha <mailtobhanup@gmail.com>
2023-05-20 19:45:00 +0530

    new program added.

---
+++ OperatorPrecedence.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a= 3, b= 4;
+    int c = ((((a*5)+b)-8)/2);//folow operator precedence
+    int d = a*5+b-8/2; /*follow BODMASS(in some case it is right) but in 
+         programming we use operator precedence*/
+    cout<<"The value of c is: "<<c<<endl; //5
+    cout<<"The value of d is: "<<d; //15
+    return 0;
+}
