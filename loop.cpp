8940c62dbe7f240df5315fef62a49c7d1bfa7f01
bhanupjha <mailtobhanup@gmail.com>
2023-05-26 09:26:10 +0530

    new program added

---
+++ loop.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+/* For loop in c++ */
+// for(int i = 0; i <= 100; i++){
+
+//     cout<<i<<endl;
+// }  
+
+//Example of infinite for loop
+// for(int i = 0; 1 <= 2; i++){
+
+//     cout<<i<<endl;
+// }  
+
+/* While loop in c++ */
+   //printing 1 to 50 using while loop
+//   int i = 1;
+//   while(i <= 50){
+//     cout<<i<<endl;
+//     i++;
+//   }
+
+  //Example of infinite while loop
+//   int i = 1;
+//   while(true){
+//     cout<<i<<endl;
+//     i++;
+//   }
+
+   int i = 10;
+   do{
+
+    cout<<i<<endl;
+    i++;
+   }while(false);
+
+return 0;
+}
---
+++ selectionControlStructure.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Tell me your age: ";
+    cin>> age;
+
+// 1. selection control structure: if else-if ladder  
+    // if((age <= 13) && (age >= 1)){
+    //     cout<<"you canot come to party";
+    // } 
+    // else if((age <= 16) && (age >= 14)){
+    //     cout<<"you are a kid and you will get a kid pass to come the party";
+    // }
+    // else if(age > 25){
+    //     cout<<"You can't come to the party";
+    // }
+    // else if(age < 1){
+    //     cout<<"You are not yet born";
+    // }
+    // else{
+    // cout<<"you come and join the party";
+    // }
+
+// 2.  selection control structure: Switch case statement
+    switch (age)
+    {
+    case 18:
+        cout<<"You are 18"<<endl;
+       // break..>  if we don't use it then next case statements will be executed until break appears.
+    case 22:
+        cout<<"You are 22"<<endl;
+        break; // The break statement ends execution of the nearest enclosing loop or conditional statement in which it appears.
+    
+    default:
+        cout<<"No such cases"<<endl;
+        break;
+    } 
+    return 0;
+}
