e86bee3f68e09c4e7a54450c5cbccca9228d3946
bhanupjha <mailtobhanup@gmail.com>
2023-05-26 17:15:08 +0530

    new program added

---
+++ PracticeLoop.cpp
@@ -0,0 +1,26 @@
+/*Write a program print table of 6 using for loop, while loop, Do while loop*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 6;
+
+//using for loop  
+    // for(int i = 1; i <= 10; i++){
+    //     cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    // }
+//using while loop
+    // int i = 1;
+    // while(i <= 10){
+    //      cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    //      i++;
+    // }
+
+//using Do while loop
+    int i = 1;
+    do{
+    cout<<n<<" * "<<i<<" = "<<i * n<<endl;
+    i++;
+    }while(i <= 10);    
+    return 0;
+}
