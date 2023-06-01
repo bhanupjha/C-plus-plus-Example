3d2b09cc4d9fab48c128507e73781345e6a8eb89
bhanupjha <mailtobhanup@gmail.com>
2023-06-01 17:43:19 +0530

    new program added

--- Union.cpp
+++ Union.cpp
@@ -13,10 +13,10 @@ union money
 int main(){
     union money m1;
     m1.rice = 25;
-    cout << "The first value at the allocated memory: " << m1.rice << endl;
-    m1.car = 34;
-    cout << "The next value is stored after removing the first value: " << m1.car << endl;
+    cout<<"The first value at the allocated memory: "<<m1.rice<<endl;
+    m1.car = 34; // print ASCII value
+    cout<<"The next value is stored after removing the first value: "<<m1.car <<endl;
     m1.pounds = 2500000;
-    cout << "The Final value value at the same allocated memory space : " << m1.pounds << endl; 
+    cout<<"The Final value value at the same allocated memory space : "<<m1.pounds<<endl; 
     return 0;
 }
